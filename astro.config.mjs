da839021fc6b099beef4426d57cdb7d329a232f6
Luke Rucker <ruckerlh@gmail.com>
2024-01-17 17:26:05 -0500

    tweak astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://lukerucker.com',
+  prefetch: {
+    defaultStrategy: 'viewport',
+  },
   integrations: [
     mdx(),
     sitemap(),
